778001949153a214341e775caf789c95b63af5ff
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-01-27 17:31:44 +0600

    Create ci.sh
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

---
+++ ci.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$CIRRUS_BRANCH" != "master" ]
+then
+    docker buildx build --platform linux/amd64,linux/arm64 \
+       --tag ghcr.io/khulnasoft/flutter:${FLUTTER_VERSION/+/-} \
+       --tag ghcr.io/khulnasoft/flutter:$DOCKER_TAG \
+       --build-arg flutter_version=$FLUTTER_VERSION \
+       sdk
+    exit 0
+fi
+
+echo $GITHUB_TOKEN | docker login ghcr.io -u fkorotkov --password-stdin
+
+docker buildx build --platform linux/amd64,linux/arm64 --push \
+   --tag ghcr.io/khulnasoft/flutter:${FLUTTER_VERSION/+/-} \
+   --tag ghcr.io/khulnasoft/flutter:$DOCKER_TAG \
+   --build-arg flutter_version=$FLUTTER_VERSION \
+   sdk
